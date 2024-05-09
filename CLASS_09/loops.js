d8fb5ab263ad2ded45b7544cdb03d050fdbfc188
Darakhshan Juniad <156326631+Darakhshan2@users.noreply.github.com>
2024-05-09 04:57:31 -0700

    Add files via upload

---
+++ CLASS_09/loops.js
@@ -0,0 +1,15 @@
+for (var i = 1; i <= 4; i++) {
+    // console.log("hello world");
+}
+var num = [1, 2, 3, 4, 5, 6, 7];
+for (var i = 0; i <= num.length; i++) {
+    // console.log("this is the count " , i);
+}
+var a = 5;
+while (a < 4) {
+    function createHello() {
+        return "hello world";
+    }
+}
+var vari = createHello();
+console.log(vari);
---
+++ CLASS_09/loops.ts
@@ -0,0 +1,8 @@
+for(let i = 1; i<=4 ; i++){
+   // console.log("hello world");
+}
+
+let num=[1,2,3,4,5,6,7];
+for(let i=0 ; i <= num.length ; i++){
+   // console.log("this is the count " , i);
+}
